29a737b11b41c9183d2c32645009ab9d3a4718ee
ljw0913 <jiawei3e1@gmail.com>
2019-03-24 10:16:14 +0800

    updated my portfolio

--- js/slideshow.js
+++ js/slideshow.js
@@ -15,3 +15,22 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+
+var slideCompIndex = 1;
+showCompDivs(slideCompIndex);
+
+function plusCompDivs(n) {
+  showCompDivs(slideCompIndex += n);
+}
+
+function showCompDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("myCompSlides");
+  if (n > x.length) {slideCompIndex = 1}    
+  if (n < 1) {slideCompIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideCompIndex-1].style.display = "block";  
+}
